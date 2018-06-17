1dcae454b9484252503e270cadb7e0fa0a8c1911
Luis Romero <diceluisco@gmail.com>
2018-06-17 15:49:48 -0500

    use live projects links

--- app.js
+++ app.js
@@ -8,37 +8,37 @@ let datos = [
 		titulo : "Diseño adaptable",
 		info : "Se realizó un diseño que se adapta a las diferentes pantallas de los dispositivos, para ello se hizo uso de las media queries en CSS.",
 		foto : "photo/foto0.png",
-		enlace : "https://github.com/unluisco/Project-2-Responsive-Layout"
+		enlace : "https://unluisco.github.io/Project-2-Responsive-Layout/"
 	},
 	{
 		titulo : "Galería de fotos interactiva",
 		info : "Para este diseño se utilizó una librería JS que nos permite implementar un carrusel, y este se usó para mostrar las diferentes fotografías.",
 		foto : "photo/foto1.png",
-		enlace : "https://github.com/unluisco/Project-4-Interactive-Photo-Gallery"
+		enlace : "https://unluisco.github.io/Project-4-Interactive-Photo-Gallery/"
 	},
 	{
 		titulo : "Reproductor de video interactivo",
 		info : "Al trabajar en el reproductor se usó una librería JS para configurar las diferentes opciones y así tener un reproductor de video de calidad profesional.",
 		foto : "photo/foto2.png",
-		enlace : "https://github.com/unluisco/Project-6-Interactive-Video-Player"
+		enlace : "https://unluisco.github.io/Project-6-Interactive-Video-Player/"
 	},
 	{
 		titulo : "Juego de adivinanzas",
 		info : "Se construyó un juego de adivinanzas utilizando diferentes métodos y funciones en JavaScript para implementar la aleatoriedad a la hora de seleccionar la frase.",
 		foto : "photo/foto3.png",
-		enlace : "https://github.com/unluisco/Project-7-Build-a-Game-Show-App"
+		enlace : "https://unluisco.github.io/Project-7-Build-a-Game-Show-App/"
 	},
 	{
 		titulo : "Panel para aplicación web",
 		info : "Para este proyecto se aplicó las técnicas de diseño avanzadas con HTML & CSS para lograr un panel que se adapta a las diferentes pantallas.",
 		foto : "photo/foto4.png",
-		enlace : "https://github.com/unluisco/Project-9-Web-App-Dashboard"
+		enlace : "https://unluisco.github.io/Project-9-Web-App-Dashboard/"
 	},
 	{
 		titulo : "Directorio de empleados",
 		info : "Al construir el directorio se hizo uso de la solicitud de datos JSON a una API con JavaScript, y usando los eventos se logró colocar esa información en las tarjetas de cada trabajador.",
 		foto : "photo/foto5.png",
-		enlace : "https://github.com/unluisco/project-10-Employee-Directory"
+		enlace : "https://unluisco.github.io/project-10-Employee-Directory/"
 	}
 ];
 
