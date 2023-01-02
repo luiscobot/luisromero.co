813d9021787f6435c6b28a84f4ba14d4017f7dae
Luis Romero <dev@luisromero.co>
2023-01-01 21:51:25 -0500

    add autoprefixer

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter()
-  }
+  },
 
   preprocess: [
-    sveltePreprocess()
+    sveltePreprocess({
+      postcss: {
+        plugins: [autoprefixer]
+      }
+    })
   ]
 };
 
