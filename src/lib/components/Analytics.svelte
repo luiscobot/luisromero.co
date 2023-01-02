557b7c0d1b9e4e447202a6022e86bfc89f637ef8
Luis Romero <dev@luisromero.co>
2023-01-02 11:16:03 -0500

    add analytics component

---
+++ src/lib/components/Analytics.svelte
@@ -0,0 +1,15 @@
+<script>
+import { PUBLIC_ANALYTICS_ID } from '$env/static/public';
+</script>
+
+<svelte:head>
+<script>
+  !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(arguments)};
+  d=s.createElement(q);d.src='//d1l6p2sc9645hc.cloudfront.net/gosquared.js';q=
+  s.getElementsByTagName(q)[0];q.parentNode.insertBefore(d,q)}(window,document
+  ,'script','_gs');
+
+  _gs('{ PUBLIC_ANALYTICS_ID }');
+  _gs('set', 'anonymizeIP', true);
+</script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
+import Analytics from '$lib/components/Analytics.svelte';
 import '$lib/styles/reset.css';
 import '$lib/styles/style.css';
 import Footer from '$lib/components/Footer.svelte';
 </script>
+<Analytics />
 
 <slot />
 
