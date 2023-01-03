d7dc76d9bfed9182bc44c631cc5d93b43b4a8075
Luis Romero <dev@luisromero.co>
2023-01-03 18:18:03 -0500

    create standalone nav component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+import Nav from '$lib/components/Nav.svelte';
 import Newsletter from '$lib/components/Newsletter.svelte';
 import Footer from '$lib/components/Footer.svelte';
 </script>
@@ -7,17 +8,9 @@ import Footer from '$lib/components/Footer.svelte';
   <title>Luis Romero &middot; Constructor digital</title>
 </svelte:head>
 
-<header>
-  <a class="logo" href="./">
-    <svg viewBox="0 0 1080 1080">
-      <g fill="none" fill-rule="evenodd">
-        <path fill="#FF4A00" d="M991.9 210.936c-51.916 140.532-75.795 220.579-71.637 240.142 4.159 19.563 32.447 88.522 84.867 206.876-54.94-1.953-108.494 2.614-160.662 13.703-117.378 24.95-171.917 56.989-220.816 67.383-6.52 1.386-10.474-1.182-11.86-7.703-6.238-29.344 104.17-87.086 100.745-103.201-1.387-6.521-11.861-7.702-31.424-3.544-88.033 18.712-162.127 54.908-328.412 90.253-19.563 4.158-31.424-3.544-35.582-23.107l-87.323-410.822c-4.158-19.563 3.544-31.424 23.107-35.582 166.285-35.345 240.38-71.54 328.412-90.253 19.563-4.158 31.424 3.544 35.582 23.107l10.396 48.907c104.335-22.177 225.871-27.563 364.608-16.159z"/>
-        <path fill="#B93600" d="M991.9 210.936c-51.916 140.532-75.795 220.579-71.637 240.142 4.159 19.563 32.447 88.522 84.867 206.876-54.94-1.953-108.494 2.614-160.662 13.703-117.378 24.95-171.917 56.989-220.816 67.383-6.52 1.386-10.474-1.182-11.86-7.703-6.238-29.344 104.17-87.086 100.745-103.201l-85.244-401.04c104.335-22.178 225.871-27.564 364.608-16.16z"/>
-        <path fill="#FF4A00" d="M329.302 913.354c-8.211 9.034-19.156 15.004-32.833 17.911-13.678 2.908-26.105 1.905-37.28-3.008a10 10 0 0 1-5.757-7.075L120.12 294.005c-7.277-34.236-36.621-27.998-43.898-62.233-6.238-29.345 19.681-75.748 53.916-83.025 34.235-7.277 76.787 24.572 83.024 53.917 7.277 34.235-22.067 40.472-14.79 74.708l133.31 627.177a10 10 0 0 1-2.381 8.805z"/>
-      </g>
-    </svg>
-  </a>
+<Nav />
 
+<header>
   <h1>Bienvenido a mi <span>jardín digital</span> donde comparto lo que estoy aprendiendo en <span>tecnología</span>.</h1>
 
   <div class="current-projects">
@@ -43,19 +36,14 @@ header {
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  min-height: 85vh;
-  padding: 10%;
+  min-height: 75vh;
+  padding: 0 10% 10%;
   background-color: #000;
   color: #fff;
   font-family: 'Geomanist', sans-serif;
 }
 
-.logo {
-  width: 50px;
-}
-
 h1 {
-  margin-top: 20px;
   margin-bottom: 20px;
   font-size: 3.5rem;
   font-weight: bold;
@@ -107,7 +95,7 @@ li svg {
 
 @media (min-width: 768px) {
   header {
-    padding: 60px;
+    padding: 0 60px 60px;
   }
 
   h1,
@@ -116,7 +104,6 @@ li svg {
   }
 
   h1 {
-    margin-top: 60px;
     font-size: 4rem;
     line-height: 4rem;
   }
